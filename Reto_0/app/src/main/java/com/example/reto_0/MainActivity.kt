67bc73fbed918476291e76e7e8bf7ef358ca8216
JulianHuertas <julian.huertas@evolutioncons.com>
2025-09-01 18:44:56 -0500

    centrado el texto

--- Reto_0/app/src/main/java/com/example/reto_0/MainActivity.kt
+++ Reto_0/app/src/main/java/com/example/reto_0/MainActivity.kt
@@ -12,6 +12,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.reto_0.ui.theme.Reto_0Theme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.ui.Alignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,11 +34,17 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Box(
+        modifier = Modifier.fillMaxSize(),           // ocupa toda la pantalla
+        contentAlignment = Alignment.Center          // centra el contenido
+    ) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
